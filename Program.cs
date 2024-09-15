7760318286e6dabe9e2295ce9993d2be67f9710e
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-09-15 15:46:35 -0500

    Basic slash command

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
 ﻿using System.Text;
 using Discord;
 using Discord.WebSocket;
-using System.Text.Json;
+using Discord.Net;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using System;
+using Discord.Interactions;
 
 namespace BotCore
 {
     public class Program
     {
         private static DiscordSocketClient _client;
+        public static Program I;
         private static ConfigWrapper _config;
+
+        #region Static Methods
 
         public static async Task Main()
         {
             _client = new DiscordSocketClient();
-
             _client.Log += Log;
+
+            I = new Program();
 
             //  You can assign your bot token to a string, and pass that in to connect.
             //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
@@ -37,6 +45,62 @@ namespace BotCore
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        #endregion
+
+        public Program()
+        {
+            _client.Ready += OnClientReady;
+        }
+
+        public async Task OnClientReady()
+        {
+            // Let's do our global command
+            var globalCommand = new SlashCommandBuilder
+            {
+                Name = "first-global-command",
+                Description = "This is my first global slash command"
+            };
+
+            await RegisterCommand(globalCommand);
+
+            _client.SlashCommandExecuted += SlashCommandHandler;
+            _client.UserCommandExecuted += UserCommandHandler;
+            var _interactionService = new InteractionService(_client.Rest);
+        }
+
+        public static async Task RegisterCommand(params SlashCommandBuilder[] commands)
+        {
+            try
+            {
+                ApplicationCommandProperties[] commandProperties = new ApplicationCommandProperties[commands.Length];
+                for (int i = 0; i < commands.Length; i++)
+                    commandProperties[i] = commands[i].Build();
+
+                await _client.BulkOverwriteGlobalApplicationCommandsAsync(commandProperties);
+                Console.WriteLine($"Registered {commands.Length} commands.");
+            }
+            catch (HttpException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(json);
+            }
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            Console.WriteLine($"{command.User.GlobalName} executed {command.Data.Name}");
+            await command.RespondAsync($"You executed {command.Data.Name}");
+        }
+        
+        private async Task UserCommandHandler(SocketUserCommand command)
+        {
+            Console.WriteLine($"{command.User.GlobalName} executed {command.Data.Name}");
+            await command.RespondAsync($"You executed {command.Data.Name}");
         }
     }
 }
